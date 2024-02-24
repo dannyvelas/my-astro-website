f26c140b0275b38ff43b28d655b4e508f588bd21
Daniel Velasquez <danivelas4@gmail.com>
2024-02-24 01:17:51 -0500

    v1 of using edge fn M netlify/edge-functions/pageViews.ts M src/layouts/BaseLayout.astro

--- netlify/edge-functions/pageViews.ts
+++ netlify/edge-functions/pageViews.ts
@@ -1,10 +1,20 @@
 import type { Config, Context } from "@netlify/edge-functions";
 
+const jokes = [
+  "My New Years resolution is to stop leaving things so late.",
+  "Child: Dad, make me a sandwich. Dad: Poof! You're a sandwich.",
+  "The invention of the wheel was what got things rolling",
+  "What kind of music do mummies like? Rap",
+  "What do you get when you cross a chicken with a skunk? A fowl smell!",
+];
+
 export default async function (
   _req: Request,
   _ctxt: Context
 ): Promise<Response> {
-  return new Response("Hello world");
+  const randomIndex = Math.floor(Math.random() * jokes.length);
+  const randomJoke = jokes[randomIndex];
+  return new Response(randomJoke);
 }
 
-export const config: Config = { path: "/test" };
+export const config: Config = { path: "/pageViews" };
