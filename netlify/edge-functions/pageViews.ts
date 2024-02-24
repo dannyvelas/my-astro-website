f196539b7607943ad4d3d659ead222821fc71267
Daniel Velasquez <danivelas4@gmail.com>
2024-02-24 02:05:55 -0500

    netlify/edge-functions/pageViews.ts: turns out that edge funtions run on deno and deno doesn't have great support for connecting to firebase to pausing on this approach

--- netlify/edge-functions/pageViews.ts
+++ netlify/edge-functions/pageViews.ts
@@ -1,7 +1,11 @@
 import type { Config, Context } from "@netlify/edge-functions";
-import { initializeApp, cert } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-import fs from "fs/promises";
+import {
+  initializeApp,
+  cert,
+} from "https://cdn.skypack.dev/firebase@8.7.0/app";
+import { getFirestore } from "https://cdn.skypack.dev/firebase@8.7.0/firestore";
+
+import fs from "node:fs/promises";
 
 const serviceAccount = await fs.readFile(
   "../../tiny-blog-database-e4e6d46e92f3.json"
