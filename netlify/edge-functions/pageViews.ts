e0cdacd0d534336e28608b82bc7ba0ab3bfb58d9
Daniel Velasquez <danivelas4@gmail.com>
2024-02-24 03:34:50 -0500

    successfully reading from supabase with private key M netlify/edge-functions/pageViews.ts M package-lock.json M package.json

--- netlify/edge-functions/pageViews.ts
+++ netlify/edge-functions/pageViews.ts
@@ -1,10 +1,22 @@
 import type { Config, Context } from "@netlify/edge-functions";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://yntmmqxawtjigczxvoas.supabase.co";
+const supabaseKey = process.env.SUPABASE_PRIVATE_KEY;
+if (!supabaseKey) {
+  throw Error("internal server error");
+}
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function (
   _req: Request,
   _ctxt: Context
 ): Promise<Response> {
-  return new Response("Hello world");
+  const { data, error } = await supabase.from("page_views").select();
+  if (error) {
+    return new Response("internal server error reading from page_views");
+  }
+  return new Response(JSON.stringify(data));
 }
 
 export const config: Config = { path: "/pageViews" };
