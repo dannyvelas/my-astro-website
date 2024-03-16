5f24e8001b60cb2c603a19c48dcad5d4719fd072
Daniel Velasquez <danivelas4@gmail.com>
2024-03-16 11:54:18 -0400

    fixed error where updated time was showing in UTC M package-lock.json M package.json M src/components/PostList.astro M src/layouts/BlogLayout.astro M src/util/time.ts

--- src/util/time.ts
+++ src/util/time.ts
@@ -1,11 +1,20 @@
+import { DateTime } from "luxon";
+
+const newYorkIANATimeZone = "America/New_York";
+
 export const minimalDate = (d: Date) =>
-  d.toLocaleDateString("en-us", { month: "short", day: "2-digit" });
+  d.toLocaleDateString("en-us", {
+    month: "short",
+    day: "2-digit",
+    timeZone: newYorkIANATimeZone,
+  });
 
 export const monthDayYear = (d: Date) =>
   d.toLocaleDateString("en-us", {
     month: "short",
     day: "2-digit",
     year: "numeric",
+    timeZone: newYorkIANATimeZone,
   });
 
 export const detailDate = (d: Date): string =>
@@ -15,12 +24,11 @@ export const detailDate = (d: Date): string =>
     year: "numeric",
     hour: "2-digit",
     minute: "2-digit",
+    timeZone: newYorkIANATimeZone,
   });
 
 // this function expects an ISO date string of the following form: 2023-11-01T00:00:00.000Z
-// this function will return a Date set to midnight of the local time
-// e.g. if local time is EDT, for the string above, this function will return Wed Nov 01 2023 00:00:00 GMT-0400 (Eastern Daylight Time)
-// CREDS: https://stackoverflow.com/a/39224282/11587741
-export const newDateLocal = (iso: string): Date => {
-  return new Date(`${iso.slice(0, 10)}T00:00`);
-};
+// this function will return a Date set to midnight of "America/New_York"
+// e.g. if "America/New_York" is in EDT, for the string above, this function will return Wed Nov 01 2023 00:00:00 GMT-0400 (Eastern Daylight Time)
+export const newDateET = (iso: string): Date =>
+  DateTime.fromISO(iso.slice(0, 10), { zone: newYorkIANATimeZone }).toJSDate();
