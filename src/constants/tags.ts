06cbc8aa5fbdcbb7e56d9d4eb487864153d7151f
Daniel Velasquez <danivelas4@gmail.com>
2024-03-17 00:25:45 -0400

    src/constants/tags.ts: made APL tag prettier

--- src/constants/tags.ts
+++ src/constants/tags.ts
@@ -1,3 +1,4 @@
+// these are the url-friendly tags
 const tags = [
   "design-patterns",
   "clojure",
@@ -7,6 +8,11 @@ const tags = [
   "apl",
 ];
 
+// by default, a url-friendly tag will be converted to a user-friendly tag by doing replace(/-/g, " ").
+// the tags that are an exception to this rule are saved in this variable
+const _tagToPrettyName: Record<string, string> = { apl: "APL" };
+
 export const getTags = () => structuredClone(tags);
 
-export const tagToPrettyName = (tag: string) => tag.replace(/-/g, " ");
+export const tagToPrettyName = (tag: string): string =>
+  _tagToPrettyName[tag] || tag.replace(/-/g, " ");
