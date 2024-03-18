0f9e40fcdc7f276cf711e42245531b5015b78698
Daniel Velasquez <danivelas4@gmail.com>
2024-03-18 19:31:14 -0400

    src/constants/tags.ts: added Clojure as proper name tag

--- src/constants/tags.ts
+++ src/constants/tags.ts
@@ -10,7 +10,10 @@ const tags = [
 
 // by default, a url-friendly tag will be converted to a user-friendly tag by doing replace(/-/g, " ").
 // the tags that are an exception to this rule are saved in this variable
-const _tagToPrettyName: Record<string, string> = { apl: "APL" };
+const _tagToPrettyName: Record<string, string> = {
+  apl: "APL",
+  clojure: "Clojure",
+};
 
 export const getTags = () => structuredClone(tags);
 
